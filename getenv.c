17ee45b70a86781737cead2df9f425b111756895
Nile Okomo <okomonile3@gmail.com>
2021-11-03 15:07:24 +0300

    Update Workspace

--- getenv.c
+++ getenv.c
@@ -138,8 +138,8 @@ void print_env(char **env)
 	i = 0;
 	while (env[i] != NULL)
 	{
-		write(STDOUT_FILENO, env[i], _strlen(env[i]));
-		write(STDOUT_FILENO, "\n", 1);
+		WRITE_OUT(env[i]);
+		WRITE_OUT("\n");
 		i++;
 	}
 }
